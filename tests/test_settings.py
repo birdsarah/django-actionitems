fa4883fdf59be7f033aea70a1d77a39232e35565
Sarah Bird <sarah@bonvaya.com>
2013-01-09 12:14:56 +0000

    Refactor test_settings with generic method set_settings()

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -3,29 +3,30 @@ from django.conf import settings
 import actionitems.settings as actionitems_settings
 from actionitems.models import ActionItem
 
+def _set_settings(name, value):
+    settings.__setattr__(name, value)
+    reload(actionitems_settings)
 
 class TestOriginModel():
     def test_origin_model_1_when_none_set_check_use(self):
-        reload(actionitems_settings)
+        _set_settings('','')
         assert actionitems_settings.USE_ORIGIN_MODEL is False
 
     def test_origin_model_2_when_none_set_check_origin_model(self):
-        reload(actionitems_settings)
+        _set_settings('','')
         with pytest.raises(AttributeError):
             actionitems_settings.ORIGIN_MODEL
 
     def test_origin_model_3_when_set_check_use(self):
-        settings.ACTIONITEMS_ORIGIN_MODEL = 'app.Model'
-        reload(actionitems_settings)
+        _set_settings('ACTIONITEMS_ORIGIN_MODEL','app.Model')
         assert actionitems_settings.USE_ORIGIN_MODEL is not False
 
     def test_origin_model_4_when_set_check_origin_model(self):
-        settings.ACTIONITEMS_ORIGIN_MODEL = 'actionitems.ActionItem'
-        reload(actionitems_settings)
+        _set_settings('ACTIONITEMS_ORIGIN_MODEL','actionitems.ActionItem')
         assert type(actionitems_settings.ORIGIN_MODEL) == type(ActionItem)
 
 
 class TestManagerList():
     def test_manager_list_1_check_default(self):
-        reload(actionitems_settings)
+        _set_settings('','')
         assert actionitems_settings.MANAGER_LIST == (('internal', 'Internal'),)
