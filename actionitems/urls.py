0130ed4588babb0102425d7a270101b407d0264d
Sarah Bird <sarah@bonvaya.com>
2013-01-11 06:59:47 -0800

    Bugfix typo in urls.py

--- actionitems/urls.py
+++ actionitems/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns('',
     url(r'^$', ActionItemListView.as_view(), name='actionitems_list'),
     url(r'add/$', ActionItemCreateView.as_view(), name='actionitems_create'),
     url(r'(?P<pk>[\d]+)/$', ActionItemUpdateView.as_view(), name='actionitems_update'),
-    url(r'(?P<pk>[\d]+)/(?P<sync>sync)/$', ActionItemUpdate.as_view(), name='actionitems_sync'),
+    url(r'(?P<pk>[\d]+)/(?P<sync>sync)/$', ActionItemUpdateView.as_view(), name='actionitems_sync'),
 )
